e9090f5aa76475b10d17919d29ffc17107d0e33b
Etash Guha <etashguha@Etashs-MacBook-Pro.local>
2018-11-21 06:30:29 -0500

    Working bottom sheet

--- app/src/main/java/com/example/etashguha/etude/Reader.java
+++ app/src/main/java/com/example/etashguha/etude/Reader.java
@@ -8,11 +8,15 @@ import android.os.Message;
 import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.github.barteksc.pdfviewer.PDFView;
 
@@ -20,7 +24,7 @@ import com.github.barteksc.pdfviewer.PDFView;
 public class Reader extends AppCompatActivity {
 
     PDFView pdfView;
-    PausePlay pausePlayState = PausePlay.PAUSED;
+    PausePlay pausePlayState;
     int pageNumber = 0;
     Dictionary dictionary;
     Screenshot screenshot;
@@ -31,28 +35,49 @@ public class Reader extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
     ProgressBar progBar;
     Activity baseActivity;
+    TextView definition;
+    BottomSheetBehavior behavior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reader);
 
+        pdfView = findViewById(R.id.pdfView);
+        progBar = findViewById(R.id.progressBar);
+        definition = findViewById(R.id.definition);
+        progBar.setVisibility(View.INVISIBLE);
+        CoordinatorLayout coordinatorLayout = findViewById(R.id.main_content);
+        View bottomSheet = coordinatorLayout.findViewById(R.id.bottom_sheet);
+
         baseActivity = this;
         player = new Player("");
         final Uri uri = getIntent().getData();
-        pdfView = findViewById(R.id.pdfView);
-        progBar = findViewById(R.id.progressBar);
-        progBar.setVisibility(View.INVISIBLE);
         firstTimePlaying = true;
         dictionaryHandler = new DictionaryHandler();
+        pausePlayState = PausePlay.PAUSED;
         StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
         StrictMode.setVmPolicy(builder.build());
 
+         behavior = BottomSheetBehavior.from(bottomSheet);
+        behavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                // React to state change
+                Log.e("onStateChanged", "onStateChanged:" + newState);
+            }
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                // React to dragging events
+                Log.e("onSlide", "onSlide");
+            }
+        });
+
+        behavior.setPeekHeight(0);
+        behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
 
         pdfView.fromUri(uri).pages(pageNumber).load();
         dictionaryHandler = new DictionaryHandler();
-        dictionary = new Dictionary(baseActivity, dictionaryHandler, "Chicken");
-        dictionary.run();
         bottomNavigationView = findViewById(R.id.bottomNavigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -89,6 +114,8 @@ public class Reader extends AppCompatActivity {
                         pageNumber++;
                         pdfView.fromUri(uri).pages(pageNumber).load();
                         sideButtonReset();
+                        dictionary = new Dictionary(baseActivity, dictionaryHandler, "Chicken");
+                        dictionary.run();
                 }
                 return false;
             }
@@ -176,7 +203,10 @@ public class Reader extends AppCompatActivity {
 
         @Override
         public void handleMessage(Message msg) {
-            Log.d("MainActivity", "response: " + msg.obj);
+            String response = (String)msg.obj;
+            definition.setText(response.substring(response.indexOf("\"definition\":\"") + 14, response.indexOf("\",\"partOfSpeech")));
+
+            behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
         }
     }
 }
